6d96111e3ebedd4b9a6453c157ccf6cf654dfc13
Tomi Kasurinen <tomi.kasurinen@solita.fi>
2020-08-28 13:36:35 +0300

    Fixed ex1.01, string is now static after initial creation.

--- project/backend/src/main.rs
+++ project/backend/src/main.rs
@@ -4,10 +4,10 @@ use std::time::Duration;
 use uuid::Uuid;
 
 fn main() {
+    let id = Uuid::new_v4();
+
     loop {
         let now = Utc::now();
-        let id = Uuid::new_v4();
-
         println!("{:?}: {}", now, id);
         sleep(Duration::new(5, 0));
     }
