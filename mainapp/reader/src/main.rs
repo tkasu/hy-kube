a349230f7180c857759bc9116641ac0352394595
Tomi Kasurinen <omi.kasurinen@gmail.com>
2020-09-07 22:18:26 +0300

    Added ex1.11.

--- mainapp/reader/src/main.rs
+++ mainapp/reader/src/main.rs
@@ -8,7 +8,9 @@ use std::env;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::ops::Deref;
 use std::path::Path;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -25,6 +27,55 @@ impl AppState {
     fn update_latest_input(&mut self, new: String) {
         self.latest_input = new;
     }
+}
+
+struct PingState {
+    count: Option<AtomicUsize>,
+}
+
+impl PingState {
+    fn init(&mut self) {
+        self.count = Some(AtomicUsize::new(0));
+    }
+
+    fn update_ping_count(&self, new: usize) {
+        self.count.as_ref().unwrap().store(new, Ordering::Relaxed);
+    }
+}
+
+fn read_ping_count(path: &Path) -> Result<usize, String> {
+    let file_opts = OpenOptions::new().read(true).open(path);
+    let file = match file_opts {
+        Err(e) => {
+            let e_msg = format!("WARNING: Couldn't open {}: {}", path.display(), e);
+            return Err(e_msg);
+        }
+        Ok(file) => file,
+    };
+
+    let mut reader = BufReader::new(file);
+
+    let mut line = String::new();
+    match reader.read_line(&mut line) {
+        Err(e) => {
+            let e_msg = format!("WARNING: Error reading {}: {}", path.display(), e);
+            return Err(e_msg);
+        }
+        Ok(_) => (),
+    }
+
+    let new_count = match line.parse::<usize>() {
+        Err(e) => {
+            let e_msg = format!(
+                "WARNING: Could not parse pingpong count from string: {}, {}",
+                line, e
+            );
+            return Err(e_msg);
+        }
+        Ok(int) => int,
+    };
+
+    return Ok(new_count);
 }
 
 fn read_lines_from(path: &Path, sender: &Sender<String>, from_line: usize) -> usize {
@@ -58,6 +109,24 @@ fn read_and_send(file_path: String, sender: Sender<String>) {
     }
 }
 
+fn update_pings(file_path: String, state: Arc<PingState>) {
+    let update_freq = Duration::from_secs(2);
+    let path = Path::new(file_path.as_str());
+    loop {
+        let new_count = match read_ping_count(path) {
+            Err(e) => {
+                println!("{}", e);
+                sleep(update_freq);
+                continue;
+            }
+            Ok(count) => count,
+        };
+
+        state.update_ping_count(new_count);
+        sleep(update_freq);
+    }
+}
+
 fn update_and_log(s: String, receiver: Receiver<String>, state: Arc<Mutex<AppState>>) {
     loop {
         match receiver.recv_timeout(Duration::from_secs(10)) {
@@ -73,16 +142,22 @@ fn update_and_log(s: String, receiver: Receiver<String>, state: Arc<Mutex<AppSta
 }
 
 #[get("/")]
-fn get_id(state: State<Arc<Mutex<AppState>>>) -> String {
-    let app_state = state.lock().unwrap();
+fn get_id(app_state: State<Arc<Mutex<AppState>>>, ping_state: State<Arc<PingState>>) -> String {
+    let app_state = app_state.lock().unwrap();
     let id = &app_state.id.clone();
     let latest_input = &app_state.latest_input;
+    let ping_count = &ping_state.count;
 
-    format!("{} {}", latest_input, id)
+    let mut resp = format!("{} {}", latest_input, id);
+    if ping_count.is_some() {
+        let ping_count = ping_count.as_ref().unwrap();
+        resp.push_str(format!("\nPing / Pongs: {:?}", ping_count).as_str());
+    }
+    resp
 }
 
-fn get_file_path(env: &str) -> String {
-    let s = match env::var(env) {
+fn get_ts_file_path() -> String {
+    let path = match env::var("MAINAPP_FILE_PATH") {
         Err(_) => {
             let test_file_path = String::from("test.txt");
             println!("Warning using test file path: {}", test_file_path);
@@ -90,12 +165,23 @@ fn get_file_path(env: &str) -> String {
         }
         Ok(path) => path,
     };
-    s
+    path
+}
+
+fn get_pingpong_file_path() -> Option<String> {
+    let path = match env::var("PINGPONG_FILE_PATH") {
+        Err(_) => {
+            println!("WARNING! No PINGPONG_FILE_PATH defined, omitting ping pong results.");
+            None
+        }
+        Ok(path) => Some(path),
+    };
+    path
 }
 
 fn main() {
-    let env_var_name: &'static str = "MAINAPP_FILE_PATH";
-    let file_path = get_file_path(env_var_name);
+    let ts_file_path = get_ts_file_path();
+    let pingpong_file_path = get_pingpong_file_path();
 
     let (file_input_sender, file_input_receiver): (Sender<String>, Receiver<String>) = channel();
 
@@ -104,21 +190,39 @@ fn main() {
 
     let default_msg = String::from("No input yet :(");
 
-    let state = AppState {
+    let app_state = AppState {
         id: id_for_state,
         latest_input: default_msg,
     };
-    let state = Arc::new(Mutex::new(state));
-    let state_for_update = state.clone();
+
+    let mut ping_state = PingState { count: None };
+    if pingpong_file_path.is_some() {
+        ping_state.init();
+    }
+
+    let app_state = Arc::new(Mutex::new(app_state));
+    let state_for_update = app_state.clone();
+
+    let ping_state = Arc::new(ping_state);
+    let ping_state_for_update = ping_state.clone();
+
+    if pingpong_file_path.is_some() {
+        thread::spawn(move || {
+            update_pings(pingpong_file_path.unwrap(), ping_state_for_update);
+        });
+    }
 
     thread::spawn(move || {
-        read_and_send(file_path, file_input_sender);
+        read_and_send(ts_file_path, file_input_sender);
     });
 
     thread::spawn(move || {
         update_and_log(id, file_input_receiver, state_for_update);
     });
 
-    let r = rocket::ignite().mount("/", routes![get_id]).manage(state);
+    let r = rocket::ignite()
+        .mount("/", routes![get_id])
+        .manage(app_state)
+        .manage(ping_state);
     r.launch();
 }
---
+++ part01/1_11/commands.md
@@ -0,0 +1,103 @@
+# Ex 1.11
+
+## Notes
+
+```bash
+k # is alias for kubectl 
+
+192.168.99.100 # is my cluster ip, as I'm using virtualbox docker-machine backend (AMD Hackintosh probelms :sadface)
+```
+
+## Building mainapp application
+
+To rebuild the same image, first checkout the correct tag:
+
+```
+git checkout ex1.11
+```
+
+In folder mainapp/
+
+```
+./docker_build_and_publish.sh ex1.11
+```
+
+## Create cluster
+
+```
+k3d cluster create --port '8082:30080@agent[0]' -p 8081:80@loadbalancer --agents 2
+```
+
+## Init filesystem
+
+```bash
+docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube
+```
+
+## Global services:
+
+In root folder:
+
+```bash
+§ k3d cluster start
+
+$ k apply -f manifests_global/
+
+persistentvolume/hy-kube-pv created
+```
+
+## Deployment
+
+In folder mainapp/:
+
+```
+
+§ k apply -f manifests/with_pingpong/
+
+deployment.apps/hy-kube-mainapp-dep created
+ingress.extensions/hy-kube-ingress created
+persistentvolumeclaim/hy-kube-mainapp-pingpong-claim created
+service/hy-kube-mainapp-svc created
+```
+
+Test
+
+```bash
+$ k get pods
+
+NAME                                   READY   STATUS    RESTARTS   AGE
+hy-kube-mainapp-dep-59758f9d4b-qbrcp   3/3     Running   0          3m5s
+
+$ k describe ingress
+
+Name:             hy-kube-ingress
+Namespace:        default
+Address:          172.18.0.4
+Default backend:  default-http-backend:80 (<error: endpoints "default-http-backend" not found>)
+Rules:
+  Host        Path  Backends
+  ----        ----  --------
+  *           
+              /           hy-kube-mainapp-svc:2345 (10.42.2.10:3000)
+              /pingpong   hy-kube-mainapp-svc:3567 (10.42.2.10:4000)
+Annotations:  traefik.ingress.kubernetes.io/rule-type: PathPrefixStrip
+Events:       <none>
+
+````
+
+```bash
+$ curl 192.168.99.100:8081 
+2020-09-07T18:31:19.219171724Z b19a543f-87ce-4c2e-a11b-8d0918be6773
+Ping / Pongs: 0%
+
+$ curl 192.168.99.100:8081/pingpong
+pong 0
+
+$ curl 192.168.99.100:8081/pingpong
+pong 1%
+
+$ curl 192.168.99.100:8081         
+2020-09-07T18:31:59.222298082Z b19a543f-87ce-4c2e-a11b-8d0918be6773
+Ping / Pongs: 2
+```
+
