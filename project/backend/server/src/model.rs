97115165d8dbc0585aca9b809aa2c674ba198237
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2022-06-14 21:11:10 +0300

    Add request and known err logging.

--- project/backend/server/src/model.rs
+++ project/backend/server/src/model.rs
@@ -1,8 +1,21 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize, Clone, Debug, sqlx::FromRow)]
 pub struct Todo {
     pub task: String,
+}
+
+impl Todo {
+    pub fn len(&self) -> usize {
+        self.task.len()
+    }
+
+    pub fn to_untyped_json(&self) -> Value {
+        let str_params = serde_json::to_string(&self).unwrap();
+        let json_params: Value = serde_json::from_str(str_params.as_str()).unwrap();
+        json_params
+    }
 }
 
 #[derive(Serialize, Clone, Debug)]
--- project/backend/server/src/server.rs
+++ project/backend/server/src/server.rs
@@ -1,29 +1,106 @@
 use crate::db;
 use crate::db::ProjectDbConn;
 use crate::model::{Todo, TodoList};
+use rocket;
 use rocket::fairing::AdHoc;
 use rocket::fs::NamedFile;
+use rocket::http::uri::Origin;
 use rocket::http::Method;
+use rocket::response::status;
 use rocket::serde::json::Json;
-use rocket::{Build, Rocket};
+use rocket::{Build, Rocket, Route};
 use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors};
 use rocket_db_pools::Database;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::net::IpAddr;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct ReqDetails<'a> {
+    details_type: &'static str,
+    method: Method,
+    uri: Origin<'a>,
+    source_ip: IpAddr,
+    params: Option<Value>,
+}
+
+impl ReqDetails<'_> {
+    fn new(route: &Route, ip: IpAddr, params: Option<Value>) -> Self {
+        Self {
+            details_type: "request",
+            method: route.method,
+            uri: route.uri.origin.clone(),
+            source_ip: ip,
+            params,
+        }
+    }
+
+    fn log(&self) {
+        let json = serde_json::json!(self).to_string();
+        println!("{:?}", json)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+enum RespErr {
+    BadRequest(String),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct ErrDetails {
+    detail_type: &'static str,
+    err: RespErr,
+}
+
+impl ErrDetails {
+    fn from_bad_req(err: &status::BadRequest<String>) -> Self {
+        let resp_err = RespErr::BadRequest(err.clone().0.unwrap());
+        Self {
+            detail_type: "error",
+            err: resp_err,
+        }
+    }
+
+    fn log(&self) {
+        let json = serde_json::json!(self).to_string();
+        println!("{:?}", json)
+    }
+}
 
 #[get("/daily_photo")]
-async fn daily_photo() -> Option<NamedFile> {
+async fn daily_photo(route: &Route, ip: IpAddr) -> Option<NamedFile> {
+    ReqDetails::new(route, ip, None).log();
     NamedFile::open("./public/assets/daily_pic.jpg").await.ok()
 }
 
 #[get("/todos")]
-async fn todos(db_conn: &ProjectDbConn) -> Json<TodoList> {
+async fn todos<'a>(db_conn: &ProjectDbConn, route: &Route, ip: IpAddr) -> Json<TodoList> {
+    ReqDetails::new(route, ip, None).log();
+
     let todos = db::get_todos(db_conn).await;
     Json(todos)
 }
 
 #[post("/todo", format = "json", data = "<todo>")]
-async fn new_todo(db_conn: &ProjectDbConn, todo: Json<Todo>) -> Json<Todo> {
+async fn new_todo<'a>(
+    todo: Json<Todo>,
+    db_conn: &ProjectDbConn,
+    route: &Route,
+    ip: IpAddr,
+) -> Result<Json<Todo>, status::BadRequest<String>> {
+    let json_params = todo.to_untyped_json();
+    ReqDetails::new(route, ip, Some(json_params)).log();
+
+    let todo_len = todo.len();
+    if todo_len > 140 {
+        let error_msg = format!("Todo's length: {} is over the limit 140.", todo_len);
+        let err_status = status::BadRequest(Some(error_msg));
+        //println!("Err Resp: {:?}", err_status);
+        ErrDetails::from_bad_req(&err_status).log();
+        return Err(err_status);
+    }
     db::add_todo(db_conn, todo.clone().0).await;
-    todo
+    Ok(todo)
 }
 
 fn get_cors() -> Cors {
