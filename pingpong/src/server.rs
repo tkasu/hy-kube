990f81861c9433a733c9999d6bc81b07c5cc2407
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2022-06-27 16:29:52 +0300

    Initial version for ex3.02.

--- pingpong/src/server.rs
+++ pingpong/src/server.rs
@@ -7,14 +7,14 @@ use rocket::{Build, Rocket};
 
 use rocket_db_pools::Database;
 
-#[get("/")]
+#[get("/pingpong")]
 async fn pong(db: &PingPongDbConn) -> String {
     let old_count = db::get_ping(db).await.unwrap();
     db::inc_ping(db).await;
     format!("pong {}", old_count.ping_count)
 }
 
-#[get("/pings")]
+#[get("/pingpong/pings")]
 async fn pings(db: &PingPongDbConn) -> Json<models::PingStatus> {
     let ping_status = db::get_ping(db).await.unwrap();
     Json(ping_status)
